45e3c2908d22177e95a3a409c4d5a3d22f6266a8
Maciej Mickiewicz <maciej.mickiewicz@bright-it.com>
2021-03-27 20:08:02 +0100

    Add rigid movement

---
+++ Assets/Scripts/SlugScript.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlugScript : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private LineRenderer ForceRay;
+  
+
+    private Vector2 StartPoint;
+    private Vector2 EndPoint;
+    private Vector2 Force;
+    private float MaxSpeed = 10f;
+    private float Speed;
+    private bool Aiming = false;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody2D>();        
+        rb.bodyType = RigidbodyType2D.Dynamic;
+
+        ForceRay = gameObject.GetComponentInChildren(typeof(LineRenderer), true) as LineRenderer;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Jump();
+        DrawRay();
+    }
+
+    void DrawRay()
+    {
+        ForceRay.gameObject.SetActive(Aiming);
+        if (Aiming)
+        {
+            var StartPointWorldRel = Camera.main.ScreenToWorldPoint(StartPoint);
+            var MousePositionWorldRel = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            ForceRay.SetPosition(0, new Vector3(StartPointWorldRel.x, StartPointWorldRel.y, 0f));
+            ForceRay.SetPosition(1, new Vector3(MousePositionWorldRel.x, MousePositionWorldRel.y, 0f));
+        }
+    }
+
+    void Jump()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            StartPoint = Input.mousePosition;
+            Aiming = true;
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            Aiming = false;
+
+            EndPoint = Input.mousePosition;
+            Speed = Mathf.Min(Vector2.Distance(EndPoint, StartPoint) / 50, MaxSpeed);
+            Force = (EndPoint - StartPoint).normalized * Speed;
+            rb.velocity = Force;
+        }
+    }
+}
