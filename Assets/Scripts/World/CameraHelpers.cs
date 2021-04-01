aaa1b1ef19558192b5ef8928512e703b6e37513d
Wojciech Gabel <wgabel34@gmail.com>
2021-04-01 23:11:45 +0200

    clean up

---
+++ Assets/Scripts/World/CameraHelpers.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraHelpers : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //public bool GetScreenPoints(out bool[] outOfScreen)
+    //{
+    //    Vector3 pos = Camera.main.WorldToViewportPoint(transform.position);
+
+    //    if (pos.x < 0.0) Debug.Log("I am left of the camera's view.");
+    //    if (1.0 < pos.x) Debug.Log("I am right of the camera's view.");
+    //    if (pos.y < 0.0) Debug.Log("I am below the camera's view.");
+    //    if (1.0 < pos.y) Debug.Log("I am above the camera's view.");
+    //    return false;
+    //}
+}
